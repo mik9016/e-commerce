7093d91de0b73f68ef4ede235573b384db17bec9
mik9016 <mikolajgruszecki@o2.pl>
2022-03-03 20:51:33 +0100

    sass installed fonts mixins reset and variables files added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div class="parent">
+    <p>Hallo</p>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
+<style lang="scss" scoped>
+@use "../assets/scss/variables.scss" as v;
+@use "../assets/scss/mixins.scss" as m;
+
+.parent {
+  @include m.flexLayout(column, center, center);
+  p {
+    color: aquamarine;
+  }
+}
+</style>
