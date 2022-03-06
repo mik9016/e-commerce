45845cd42ff57de9b7b186862711f790e8f2fe8d
mik9016 <mikolajgruszecki@o2.pl>
2022-03-06 01:01:29 +0100

    Landing page added, item design changed

--- components/Header.vue
+++ components/Header.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    <h1>{{ c.header.title }}</h1>
+  <div class="head">
+    <nuxt-link to="/">
+      <button>
+        <h1>{{ c.header.title }}</h1>
+      </button></nuxt-link
+    >
+    <hr class="head__line" />
   </div>
 </template>
+
 <script>
 import c from "../content/text.json";
 export default {
@@ -18,9 +24,26 @@ export default {
 @use "../assets/scss/variables.scss" as v;
 @use "../assets/scss/mixins.scss" as m;
 
-h1 {
-  font-family: Montserrat, sans-serif;
-  font-size: 4rem;
-  @include m.flexLayout(row, center, center);
+.head {
+  background-color: transparent;
+  position: absolute;
+  width: 100%;
+  top: 0;
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    margin: 0;
+  }
+  h1 {
+    margin: 1rem;
+    font-family: Lato, sans-serif;
+    font-size: 1.5rem;
+    color: v.$black;
+    @include m.flexLayout(row, center, flex-start);
+  }
+  &__line {
+    color: v.$black;
+  }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <Header />
-    <Items />
+    <Landing />
   </div>
 </template>
-
